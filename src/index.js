5e4bddbdd3309e9f8a4e964314f13c7120674423
Shaunmak1214 <shaonmak@gmail.com>
2021-08-30 11:38:12 +0800

    feat: initial commit ❤️

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
+// import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import createHistory from 'history/createBrowserHistory';
+import { Router } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './store/store';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    primary: '#ABC4FF',
+    accent: '#F2F6FF',
+    secondary: '#D9F3F0',
+    highlight: '#00C9A7',
+    warning: '#FFC75F',
+    danger: '#C34A36',
+  },
+  components: {
+    Button: {
+      variants: {
+        solid() {
+          return {
+            _hover: {
+              bg: `#BBC7E4`,
+            },
+            _active: { bg: `#BBC7E4` },
+          };
+        },
+      },
+    },
+  },
+});
+
+const history = createHistory();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Provider store={store}>
+      <Router history={history}>
+        <ChakraProvider theme={theme}>
+          <App />
+        </ChakraProvider>
+      </Router>
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
---
+++ src/reducers/authSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+import cookie from 'js-cookie';
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    user:
+      cookie.get('user') !== undefined ? JSON.parse(cookie.get('user')) : {},
+    isAuthenticated: cookie.get('accessToken') ? true : false,
+    accessToken: cookie.get('accessToken') ? cookie.get('accessToken') : '',
+  },
+  // fix all this
+  reducers: {
+    LOGIN: (state, action) => {
+      state.user = action.payload.user;
+      state.accessToken = action.payload.accessToken;
+      state.isAuthenticated = true;
+      cookie.set('user', JSON.stringify(action.payload.user));
+      cookie.set('isAuthenticated', true);
+      cookie.set('accessToken', action.payload.accessToken);
+    },
+    LOGOUT: (state) => {
+      state.isAuthenticated = false;
+      state.accessToken = '';
+      state.user = null;
+      cookie.set('user', null);
+      cookie.set('isAuthenticated', false);
+      cookie.set('accessToken', '');
+    },
+    UPDATE: (state, action) => {
+      state.user = action.payload;
+    },
+  },
+});
+
+export const { LOGIN, LOGOUT, UPDATE_USER } = authSlice.actions;
+
+export const selectIsAuthenticated = (state) => state.isAuthenticated;
+export const selectUser = (state) => state.user;
+export default authSlice.reducer;
