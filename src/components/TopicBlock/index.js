0ca4e7d179f31fca6bd6e603cb0ed8d3169dae4d
Shaun Mak <60981304+Shaunmak1214@users.noreply.github.com>
2021-09-15 19:29:55 +0800

    feat: speaker names, better ux

--- src/components/TopicBlock/index.js
+++ src/components/TopicBlock/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { VStack, HStack, Text, Box, Flex } from '@chakra-ui/layout';
+import { VStack, HStack, Text, Box, Flex, Center } from '@chakra-ui/layout';
 import { Image } from '@chakra-ui/image';
 
 import TopicBadge from './../TopicBadge';
 
-import { ChampionIcon, SilverIcon, BronzeIcon } from '../../assets';
+import {
+  ChampionIcon,
+  SilverIcon,
+  BronzeIcon,
+  SpeakerIcon,
+} from '../../assets';
 
 const TopicBlock = ({
   rounded,
   topic,
+  speaker,
   lead,
   themeIcon,
   count,
@@ -44,16 +50,16 @@ const TopicBlock = ({
   return (
     <HStack
       w="100%"
-      p="15px 25px"
+      p="10px 25px"
       border="1px solid #E9E9E9;"
       borderRadius="8px"
-      mb="2"
-      mt="15px"
+      mb="10px"
+      mt="0"
       cursor="pointer"
       transition="all 0.1s ease-in-out"
       justifyContent="space-between"
       alignItems="center"
-      boxShadow="0px 16px 40px rgba(195, 195, 195, 0.25)"
+      boxShadow="0px 16px 40px rgba(195, 195, 195, 0.05)"
     >
       <Box width="100%">
         <HStack spacing={[0, 0, 7]} py="0.3em" px={['0rem', '0rem', '0.5em']}>
@@ -66,17 +72,31 @@ const TopicBlock = ({
           />
           <VStack align="flex-start" w="100%" wordBreak="break-all">
             <Flex
-              justifyContent="space-between"
-              w={['50%', '50%', '95%']}
+              justifyContent="flex-start"
+              w={['100%', '50%', '95%']}
               flexDir={['column', 'column', 'row']}
+              mb="10px"
             >
               <TopicBadge topic={topic.theme} />
+              <Center
+                ml={[0, 0, '10px']}
+                mt={['10px', '10px', 0]}
+                borderRadius="8px"
+                border="1px solid #e9e9e9"
+                px="3"
+              >
+                <Image src={SpeakerIcon} mr="5px" height="10px" width="auto" />
+                <Text fontSize="sm" color="#797979">
+                  {speaker.fullName}
+                </Text>
+              </Center>
             </Flex>
             <Text
               as="h3"
               fontSize="md"
               fontFamily="Montserrat"
               fontWeight="600"
+              wordBreak="break-word"
             >
               {topic.name}
             </Text>
@@ -106,6 +126,7 @@ TopicBlock.propTypes = {
   idx: PropTypes.number,
   rounded: PropTypes.bool,
   topic: PropTypes.object,
+  speaker: PropTypes.object,
   themeIcon: PropTypes.any,
   count: PropTypes.number,
   leaderboard: PropTypes.bool,
