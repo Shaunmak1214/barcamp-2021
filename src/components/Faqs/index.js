7d162b1834a7d210f98a7bc1b0f1f31be653e860
Shaun Mak <60981304+Shaunmak1214@users.noreply.github.com>
2021-09-16 15:35:45 +0800

    feat: updated

--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -43,6 +43,21 @@ const Index = ({ ...props }) => {
     },
     {
       idx: 3,
+      identifier: 'proposingTopic',
+      title: 'Proposing Topic',
+    },
+    {
+      idx: 4,
+      identifier: 'votingTopic',
+      title: 'Voting Topic',
+    },
+    {
+      idx: 5,
+      identifier: 'gatherTown',
+      title: 'Gather Town',
+    },
+    {
+      idx: 6,
       identifier: 'discordServer',
       title: 'Discord Server',
     },
@@ -60,42 +75,77 @@ const Index = ({ ...props }) => {
         idx: 1,
         title: 'Do I have to open my camera and microphone?',
         content:
-          'Throughout the event, you do not have to turn on your camera and microphone unless you are voted as a speaker based on the topic that you have proposed. ',
+          'Throughout the event, you do not have to turn on your camera and microphone, although we definitely encourage you to do so! Of course, if you are to speak in your slot, you should open your camera and microphone so that everyone can see and hear you!',
       },
       {
         idx: 2,
-        title: 'I am not added into the WhatsApp Group. What should I do?',
+        title: 'Is there any entry fee?',
         content:
-          'In the event that we do not manage to add you into our WhatsApp Group, you may join the group with the link inside our Event Handbook. You could find it from the dashboard after log in yourself as a participant. ',
+          'No, it is free. We do not collect any entry fee from participants for this event!',
       },
       {
         idx: 3,
-        title: 'How will I be able to retrieve my prizes?',
-        content:
-          'The organising committee will message the respective parties for further contact.',
-      },
-      {
-        idx: 4,
-        title: 'Is this free?',
-        content: 'Yes, it is free. We do not collect any fees for this event!',
-      },
-      {
-        idx: 5,
         title: 'How do I register as a participant?',
         content:
           'Click the "Join Us Now" button in this website and log in with your Google account. ',
       },
       {
-        idx: 6,
-        title: 'Why do I have to join the Discord server?',
+        idx: 4,
+        title: 'Is it necessary for me to present a topic during BarCamp?',
         content:
-          'For BarCamp Cyberjaya 2021, we will be using a Discord server as our main channel to upload our event announcements and materials to participants, as well as encouraging participants to communicate with each other and get familiar with this social platform.',
+          'If this is your first time attending BarCamp, we highly encourage you to give yourself a chance and come up with a topic which you can present to the audience! Of course, if you do not want to, that is perfectly fine as well.',
+      },
+      {
+        idx: 5,
+        title:
+          'How do I know if my topic is getting into the slots of BarCamp 2021?',
+        content:
+          'The voting for the topics will be open from 25th September to 30th September 2021. The 15 topics with the most votes will be announced on the next day, with their specified time slots, so make sure to check back on the 1st of October to see if your topic was voted!',
+      },
+      {
+        idx: 6,
+        title:
+          'My topic was voted and a slot has been allocated for me, but I have an emergency and cannot make it on the day of the event. What should I do?',
+        content:
+          'Feel free to contact our Barcamp team via itsoc.mmu@gmail.com to assist you further on this matter. ',
       },
       {
         idx: 7,
+        title: 'How do I register on Gather.Town?',
+        content:
+          'Just click into the Gather.Town link (https://www.gather.town/) and click on the ‘Sign Up’ button on the top right corner of the screen. ',
+      },
+      {
+        idx: 8,
+        title: 'Do I have to install the desktop app for Gather.Town? ',
+        content:
+          'No, you do not have to install the desktop app for Gather.Town. You can just log into the website to enter BarCamp Cyberjaya 2021 Server after registering on Gather.Town.',
+      },
+      {
+        idx: 9,
+        title:
+          'I cannot access the Gather.Town BarCamp Cyberjaya 2021 Server, what should I do?',
+        content:
+          'Feel free to contact our Barcamp team via itsoc.mmu@gmail.com to assist you further on this matter. ',
+      },
+      {
+        idx: 10,
+        title:
+          'Can I use any of the rooms in Gather.Town during the day of the event?',
+        content:
+          'Yes you can! Feel free to use any empty space in Gather.Town to hang out with your friends! Only the spaces in the 3 Halls are reserved for proposed topics to be shared. Any other place is free for you to explore and stay in.',
+      },
+      {
+        idx: 11,
+        title: 'Why do I have to join the Discord server?',
+        content:
+          'For BarCamp Cyberjaya 2021, we will be using a Discord server as our main channel to upload our event announcements to keep you updated with the latest news, as well as encouraging everyone to communicate and make friends with each other before the day of the event. ',
+      },
+      {
+        idx: 12,
         title: 'What can I do inside the Discord Server?',
         content:
-          'You can chat and also meet up with anyone inside the Discord Server! Make yourself at home and make some new friends too! Just make sure to follow our Discord Server Rules so that everyone can have a pleasant experience.',
+          'You can chat and also meet up with anyone inside the Discord Server! Make yourself at home and make some new friends too! Just make sure to follow our Discord Server Rules so that everyone can have a pleasant experience. ',
       },
     ],
     general: [
@@ -103,32 +153,28 @@ const Index = ({ ...props }) => {
         idx: 0,
         title: 'What do I have to prepare for this event?',
         content:
-          'The first thing you need to do is to create an account for Gather Town, and familiarize yourself with the features of the platform before the event day itself to ensure a smooth session. Secondly, do join this event with the intent of sharing your knowledge with others <3.',
+          'The first thing you need to do is to create an account for Gather.Town  and familiarize yourself with the features of the platform before the event day itself to ensure a smooth session. Secondly, do join this event with the intent of sharing your knowledge with others!',
       },
       {
         idx: 1,
         title: 'Do I have to open my camera and microphone?',
         content:
-          'Throughout the event, you do not have to turn on your camera and microphone unless you are voted as a speaker based on the topic that you have proposed. ',
+          'Throughout the event, you do not have to turn on your camera and microphone, although we definitely encourage you to do so! Of course, if you are to speak in your slot, you should open your camera and microphone so that everyone can see and hear you!',
       },
       {
         idx: 2,
-        title: 'I am not added into the WhatsApp Group. What should I do?',
+        title:
+          'In an ongoing session, I realise I do not enjoy it. Can I leave halfway and join another ongoing session? ',
         content:
-          'In the event that we do not manage to add you into our WhatsApp Group, you may join the group with the link inside our Event Handbook. You could find it from the dashboard after log in yourself as a participant. ',
-      },
-      {
-        idx: 3,
-        title: 'How will I be able to retrieve my prizes?',
-        content:
-          'The organising committee will message the respective parties for further contact.',
+          'Yes of course you can! We would like to stress that on the day of the event you get to VOTE WITH YOUR FEET, meaning you can either enter or leave a hall anytime you please to join another topic you may find interesting as well!',
       },
     ],
     registration: [
       {
         idx: 0,
-        title: 'Is this free?',
-        content: 'Yes, it is free. We do not collect any fees for this event!',
+        title: 'Is there any entry fee?',
+        content:
+          'No, it is free. We do not collect any entry fee from participants for this event!',
       },
       {
         idx: 1,
@@ -137,18 +183,90 @@ const Index = ({ ...props }) => {
           'Click the "Join Us Now" button in this website and log in with your Google account. ',
       },
     ],
+    proposingTopic: [
+      {
+        idx: 0,
+        title: 'Is it necessary for me to present a topic during BarCamp?',
+        content:
+          'If this is your first time attending BarCamp, we highly encourage you to give yourself a chance and come up with a topic which you can present to the audience! Of course, if you do not want to, that is perfectly fine as well.',
+      },
+      {
+        idx: 1,
+        title:
+          'How do I know if my topic is getting into the slots of BarCamp 2021?',
+        content:
+          'The voting for the topics will be open from 25th September to 30th September 2021. The 15 topics with the most votes will be announced on the next day, with their specified time slots, so make sure to check back on the 1st of October to see if your topic was voted!',
+      },
+      {
+        idx: 2,
+        title:
+          'My topic was voted and a slot has been allocated for me, but I have an emergency and cannot make it on the day of the event. What should I do?',
+        content:
+          'Feel free to contact our Barcamp team via itsoc.mmu@gmail.com to assist you further on this matter. ',
+      },
+    ],
+    votingTopic: [
+      {
+        idx: 0,
+        title: 'What is the voting session conducted for?',
+        content:
+          'The voting for the topics will be conducted starting from 25th September to 30th September 2021. The 15 topics with the most votes will be selected to share on the day of Barcamp, the finalized list of ',
+      },
+      {
+        idx: 1,
+        title:
+          'How do I know if my topic is getting into the slots of BarCamp 2021?',
+        content:
+          'The voting for the topics will be open from 25th September to 30th September 2021. The 15 topics with the most votes will be announced on the next day, with their specified time slots, so make sure to check back on the 1st of October to see if your topic was voted!',
+      },
+      {
+        idx: 2,
+        title:
+          'My topic was voted and a slot has been allocated for me, but I have an emergency and cannot make it on the day of the event. What should I do?',
+        content:
+          'Feel free to contact our Barcamp team via itsoc.mmu@gmail.com to assist you further on this matter. ',
+      },
+    ],
+    gatherTown: [
+      {
+        idx: 0,
+        title: 'How do I register on Gather.Town?',
+        content:
+          'Just click into the Gather.Town link (https://www.gather.town/) and click on the ‘Sign Up’ button on the top right corner of the screen. ',
+      },
+      {
+        idx: 1,
+        title: 'Do I have to install the desktop app for Gather.Town? ',
+        content:
+          'No, you do not have to install the desktop app for Gather.Town. You can just log into the website to enter BarCamp Cyberjaya 2021 Server after registering on Gather.Town.',
+      },
+      {
+        idx: 2,
+        title:
+          'I cannot access the Gather.Town BarCamp Cyberjaya 2021 Server, what should I do?',
+        content:
+          'Feel free to contact our Barcamp team via itsoc.mmu@gmail.com to assist you further on this matter. ',
+      },
+      {
+        idx: 3,
+        title:
+          'Can I use any of the rooms in Gather.Town during the day of the event?',
+        content:
+          'Yes you can! Feel free to use any empty space in Gather.Town to hang out with your friends! Only the spaces in the 3 Halls are reserved for proposed topics to be shared. Any other place is free for you to explore and stay in.',
+      },
+    ],
     discordServer: [
       {
         idx: 0,
         title: 'Why do I have to join the Discord server?',
         content:
-          'For BarCamp Cyberjaya 2021, we will be using a Discord server as our main channel to upload our event announcements and materials to participants, as well as encouraging participants to communicate with each other and get familiar with this social platform.',
+          'For BarCamp Cyberjaya 2021, we will be using a Discord server as our main channel to upload our event announcements to keep you updated with the latest news, as well as encouraging everyone to communicate and make friends with each other before the day of the event. ',
       },
       {
         idx: 1,
         title: 'What can I do inside the Discord Server?',
         content:
-          'You can chat and also meet up with anyone inside the Discord Server! Make yourself at home and make some new friends too! Just make sure to follow our Discord Server Rules so that everyone can have a pleasant experience.',
+          'You can chat and also meet up with anyone inside the Discord Server! Make yourself at home and make some new friends too! Just make sure to follow our Discord Server Rules so that everyone can have a pleasant experience. ',
       },
     ],
   };
---
+++ src/components/Float/index.js
@@ -0,0 +1,37 @@
+import { HStack, Link, Text } from '@chakra-ui/layout';
+import { Image } from '@chakra-ui/react';
+import React from 'react';
+
+import { DiscordOutlinedIcon } from '../../assets';
+
+const Index = () => {
+  return (
+    <HStack position="fixed" bottom="20px" right="20px" zIndex="1001">
+      <Link
+        _hover={{ bg: 'black' }}
+        bg="#5865F2"
+        px="5"
+        py="2"
+        borderRadius="50px"
+        color="white"
+        d="flex"
+        flexDir="row"
+        alignItems="center"
+        href="#"
+      >
+        <Image
+          src={DiscordOutlinedIcon}
+          h="25px"
+          w="25px"
+          mr="10px"
+          alt="Discord icon"
+        />{' '}
+        <Text fontFamily="Montserrat" fontSize="15px">
+          Join our discord
+        </Text>
+      </Link>
+    </HStack>
+  );
+};
+
+export default Index;
