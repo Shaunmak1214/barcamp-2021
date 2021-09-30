6415145c900051722b6e3002fc00016e05d3e5e2
JasonTanz <65846113+JasonTanz@users.noreply.github.com>
2021-10-01 00:41:52 +0800

    fix/remove-revert-button

--- src/screens/dashboard.jsx
+++ src/screens/dashboard.jsx
@@ -11,7 +11,7 @@ import {
   Flex,
 } from '@chakra-ui/layout';
 
-import { PrimaryButton, RevertButton } from '../components/Buttons';
+import { PrimaryButton } from '../components/Buttons';
 import BCSpacer from '../components/Spacer';
 import { SectionTitle } from 'components/SectionTitle';
 import InfoBlock from 'components/InfoBlock';
@@ -416,28 +416,6 @@ const Dashboard = () => {
               <SectionTitle fontSize="2xl" type="left" mb="10">
                 Your Voted Topic
               </SectionTitle>
-              {votedTopics.length > 0 ? (
-                <>
-                  <RevertButton onOpen={onModalOpen}>
-                    <Text>Revert Your Voted Topics</Text>
-                  </RevertButton>
-                  <BCModal
-                    theme="normal"
-                    content={
-                      <>
-                        <Text as="h3" fontSize="xl" fontFamily="600">
-                          Are you sure you want to revert your votes?
-                        </Text>
-                      </>
-                    }
-                    modalOpen={isOpen}
-                    onClose={onModalClose}
-                    dialog={true}
-                    onDeleteVotes={deleteUserVotes}
-                    loading={deleteLoading}
-                  />
-                </>
-              ) : null}
             </Flex>
           </Container>
 
